e4f4ec851c56775c3e7a8926738a1b9fa1a22b84
Iffan Pathan <vbaako1@hotmail.com>
2021-06-29 17:08:16 +0700

    redux sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import User from "./User";
+import { connect } from "react-redux";
 
-function App() {
+//------------------------------- One State----------------------
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "ADD":
+//       state += action.payload;
+//       break;
+
+//     case "SUB":
+//       state -= action.payload;
+//       break;
+//       default:
+//   }
+//   return state
+// }
+// //createStore(reducer, state)
+// const store = createStore(reducer, 25000);
+// // แสดงผล update ค่า state ดูว่า state ที่เราต้องการเป็นค่าอะไร
+// store.subscribe(() =>{
+//   console.log("Updata Store:",store.getState());
+// })
+// //เปลี่ยนค่าใน state
+// store.dispatch({
+//   type:"ADD",
+//   payload:500
+// })
+
+// store.dispatch({
+//   type:"SUB",
+//   payload : 5000
+// })
+//------------------------------- multiple State----------------------
+// import { createStore, combineReducers } from 'redux'; //combineReducers ทำให้ Reducers หลาย ๆ ตัว ใส่ไปใน store
+
+// state 2 ตัว
+// const initialState = {
+//   num: 25000,
+//   arr: []
+// }
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "ADD":
+//       //--------------แบบไม่ย่อ------------------
+//       // state = {
+//       //   //สนใจ state ที่เป็น num
+//       //   salary: state.num += action.payload,
+//       //   //สนใจ state ที่เป็น array
+//       //   value: state.arr 
+//       // }
+//       //----------------------------------------
+//       //ยุปให้เรียกใช้ทีเดียว แบบย่อ
+//       state = {
+//           //แสดง obj ทุกตัว
+//         ...state ,
+//         // สร้างแอคทีบิว เก็บค่า num ที่เปลี่ยนแปลง
+//         salary:state.num += action.payload,
+//         // เก็บ salary ใน array
+//         // ...state.arr ต่ำแหน่งแรกของ array ซึ่งเก็บ action.payload ไว้
+//         array :[...state.arr,action.payload],
+//       }
+//       break;
+
+//     case "SUB":
+//       state = {
+//         ...state,
+//       salary:state.num -= action.payload,
+//       array :[...state.arr,action.payload],
+//     }
+//       break;
+//     default:
+//   }
+//   return state
+// }
+
+// const store = createStore(reducer);
+
+// store.subscribe(() => {
+//   console.log("Updata Store:", store.getState());
+// })
+
+// store.dispatch({
+//   type: "ADD",
+//   payload: 5000
+// })
+
+// store.dispatch({
+//   type: "SUB",
+//   payload: 10000
+// })
+
+//------------------------App.js------------------------------------
+
+const App =(props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <User  username = {props.user.name} />
     </div>
+
   );
 }
+  //นำ state ออกมาเป็น props
+const mapStatetoProps = (state) => {
+  return {
+    emp: state.emp,
+    user: state.user,
+  }
+  
+}
 
-export default App;
+export default connect(mapStatetoProps)(App);
+
+
--- src/index.js
+++ src/index.js
@@ -3,10 +3,74 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux'; //combineReducers ทำให้ Reducers หลาย ๆ ตัว ใส่ไปใน store
+
+// ------------------------------- multiple reducer----------------------
+const initialState = {
+  num: 25000,
+  arr: []
+}
+
+
+const employeeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD":
+
+      state = {
+        ...state,
+        salary: state.num += action.payload,
+        array: [...state.arr, action.payload],
+      }
+      break;
+
+    case "SUB":
+      state = {
+        ...state,
+        salary: state.num -= action.payload,
+        array: [...state.arr, action.payload],
+      }
+      break;
+    default:
+  }
+  return state
+}
+
+const userReducer = (state = { name: "john",age: 15 , }, action) => {
+  switch (action.type) {
+    case "setName":
+      state = {
+        ...state,
+        re_name: action.payload,
+      }
+      break;
+    case "setAge":
+      state = {
+        ...state,
+        re_age:action.payload,
+      }
+      console.log(state);
+      break;
+      default:
+  }
+  return state
+}
+
+// มี Reducer เดียว ใช้ store = createStore (Reducer)
+// มีหลาย Reducer ใชฟังก์ชั่น store = createStore = (combineReducers ( {ReducerA ,ReducerB} ) ) ;
+const store = createStore(combineReducers({ emp: employeeReducer, user: userReducer }));
+
+store.subscribe(() => {
+  console.log("Updata Store:", store.getState());
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     {/* Provider = นำ state มาใช้กับ component */}
+    <Provider store = {store}>
+      <App />
+    </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +79,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
